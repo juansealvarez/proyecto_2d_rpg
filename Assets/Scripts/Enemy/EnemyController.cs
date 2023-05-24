bd5691181fd12fcaefe38c7072e2d203f8075720
L34307 <L34307@lab.aca>
2023-05-24 16:42:32 -0500

    initial commit

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyController : MonoBehaviour
+{
+    #region Public Properties
+    public float WakeDistance = 5f;
+    public float Speed = 2f;
+    public float AttackDistance = 1f;
+
+    #endregion
+
+    #region Components
+    public Transform Player;
+    public SpriteRenderer spriteRenderer {private set; get; }
+    public Rigidbody2D rb { private set; get; }
+    public Animator animator { private set; get; }
+
+    public bool AttackingEnd { set; get; } = false;
+    public Transform hitBox { private set; get; }
+
+    #endregion
+
+    #region Private Properties
+    private FSM<EnemyController> mFSM;
+
+    #endregion
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        hitBox = transform.Find("Hitbox");
+
+        // Creo la maquina de estados finita
+        mFSM = new FSM<EnemyController>(new Enemy.IdleState(this));
+        mFSM.Begin(); // prendo la maquina de estados
+    }
+
+    private void FixedUpdate()
+    {
+        mFSM.Tick(Time.deltaTime);
+    }
+
+    public void SetAttackingEnd ()
+    {
+        AttackingEnd = true;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 4f;
+
+    private Rigidbody2D mRb;
+    private Vector3 mDirection = Vector3.zero;
+    private Animator mAnimator;
+    private PlayerInput mPlayerInput;
+    private Transform hitBox;
+
+    private void Start()
+    {
+        mRb = GetComponent<Rigidbody2D>();
+        mAnimator = GetComponent<Animator>();
+        mPlayerInput = GetComponent<PlayerInput>();
+
+        hitBox = transform.Find("HitBox");
+
+        ConversationManager.Instance.OnConversationStop += OnConversationStopDelegate;
+    }
+
+    private void OnConversationStopDelegate()
+    {
+        mPlayerInput.SwitchCurrentActionMap("Player");
+    }
+
+    private void Update()
+    {
+        if (mDirection != Vector3.zero)
+        {
+            mAnimator.SetFloat("Horizontal", mDirection.x);
+            mAnimator.SetFloat("Vertical", mDirection.y);
+            mAnimator.SetBool("IsMoving", true);
+        }else
+        {
+            // Quieto
+            mAnimator.SetBool("IsMoving", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        mRb.MovePosition(
+            transform.position + (mDirection * speed * Time.fixedDeltaTime)
+        );
+    }
+
+    public void OnMove(InputValue value)
+    {
+        mDirection = value.Get<Vector2>().normalized;
+    }
+
+    public void OnNext(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            ConversationManager.Instance.NextConversation();
+        }
+    }
+
+    public void OnCancel(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            ConversationManager.Instance.StopConversation();
+        }
+    }
+
+    public void OnAttack(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            mAnimator.SetTrigger("Attack");
+            hitBox.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Conversation conversation;
+        if (other.transform.TryGetComponent<Conversation>(out conversation))
+        {
+            mPlayerInput.SwitchCurrentActionMap("Conversation");
+            ConversationManager.Instance.StartConversation(conversation);
+        }
+    }
+
+    public void DisableHitBox()
+    {
+        hitBox.gameObject.SetActive(false);
+    }
+}
