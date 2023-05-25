724f14991ec00ae64ef18a692282a07311bb6508
juansealvarez <juansealvarez5@gmail.com>
2023-05-25 12:40:42 -0500

    add hitbox detector

---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the weapon zone");
+        }
+    }
+}
